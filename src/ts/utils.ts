4ff2ec0b977ea432b9195aa05d3272f5fc53b8e7
inmare <yatsumehole@naver.com>
2024-12-19 02:46:55 +0900

    이미지 로드 함수 삭제

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,25 +1,6 @@
 import * as PIXI from "pixi.js";
 import { videoSize } from "./config";
 
-export function loadImage(url: string): Promise<HTMLCanvasElement> {
-  return new Promise((resolve, reject) => {
-    const image = new Image();
-    image.onload = () => {
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext("2d");
-      if (!context) {
-        reject("Canvas context is null");
-        return;
-      }
-      canvas.width = image.width;
-      canvas.height = image.height;
-      context.drawImage(image, 0, 0);
-      resolve(canvas);
-    };
-    image.src = url;
-  });
-}
-
 /**
  * 이미지의 넓이가 영상의 넓이에 맞을 수 있도록 비율을 계산하는 함수
  * @param image 이미지 텍스처
