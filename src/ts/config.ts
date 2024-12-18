63f54e18d1696381a6e708b8eafb4abdb5b637db
inmare <yatsumehole@naver.com>
2024-12-19 02:46:29 +0900

    label 설정 객체의 구조 변경

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -6,15 +6,12 @@ const videoSize = {
 const previewLabel = {
   bgImage: "Background Image",
   audioSpectrum: "Audio Spectrum",
-  imageContainer: {
-    container: "Image Container",
-    image: "Image",
-  },
+  imageContainer: "Image Container",
+  maskContainer: "Mask Container",
+  image: "Image",
   gradient: "Gradient",
-  textContainer: {
-    container: "Text Container",
-    // TODO: text의 경우에는 기존의 설정이 따로 있고, 그걸 가지고 와야 됨
-  },
+  textContainer: "Text Container",
+  // TODO: text의 경우에는 기존의 설정이 따로 있고, 그걸 가지고 와야 됨
 } as const;
 
 export { videoSize, previewLabel };
