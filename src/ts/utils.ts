15bc5ce94619775bc898b8327af04ecc19d9a4fa
inmare <yatsumehole@naver.com>
2024-12-18 19:56:50 +0900

    이미지 비율 계산 함수 추가

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,3 +1,6 @@
+import * as PIXI from "pixi.js";
+import { videoSize } from "./config";
+
 export function loadImage(url: string): Promise<HTMLCanvasElement> {
   return new Promise((resolve, reject) => {
     const image = new Image();
@@ -15,4 +18,13 @@ export function loadImage(url: string): Promise<HTMLCanvasElement> {
     };
     image.src = url;
   });
+}
+
+/**
+ * 이미지의 넓이가 영상의 넓이에 맞을 수 있도록 비율을 계산하는 함수
+ * @param image 이미지 텍스처
+ * @returns 이미지의 넓이에 곱하면 영상의 넓이가 되는 비율
+ */
+export function calculateRatio(image: PIXI.Texture): number {
+  return videoSize.width / image.width;
 }
