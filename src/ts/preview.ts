03b8f399d896c16d698f4990f8065811c83031cf
inmare <yatsumehole@naver.com>
2024-12-18 19:58:17 +0900

    init함수에 마스크 및 메서드 추가

--- src/ts/preview.ts
+++ src/ts/preview.ts
@@ -2,41 +2,99 @@ import * as PIXI from "pixi.js";
 import { DropShadowFilter } from "pixi-filters";
 import DefaultImage from "@assets/images/DefaultImage.png";
 import { TextInput } from "@ts/song";
-import { loadImage } from "@ts/utils";
+import { loadImage, calculateRatio } from "@ts/utils";
+import { videoSize, previewLabel } from "@ts/config";
 
-export class Preview {
-  bgImageLabel: string;
-  imageLabel: string;
-  textContainerLabel: string;
-
-  constructor() {
-    this.bgImageLabel = "Background Image";
-    this.imageLabel = "Image";
-    this.textContainerLabel = "Text Container";
-  }
-
+export default class Preview {
   async init(app: PIXI.Application, canvas: HTMLCanvasElement) {
     app.init({
       canvas: canvas,
-      width: 1920,
-      height: 1080,
+      width: videoSize.width,
+      height: videoSize.height,
       backgroundColor: 0x000000,
+      antialias: true,
     });
 
     const canvasImage = await loadImage(DefaultImage);
     const defaultImage = PIXI.Texture.from(canvasImage);
     const bgSprite = new PIXI.Sprite(defaultImage);
-    bgSprite.label = this.bgImageLabel;
-    bgSprite.scale.set(1.5);
+    bgSprite.label = previewLabel.bgImage;
+    const factor = calculateRatio(defaultImage);
+    bgSprite.scale.set(factor);
     app.stage.addChild(bgSprite);
 
+    const mask = new PIXI.Graphics();
+    mask.fillStyle = 0xffffff;
+    mask.moveTo(0, 0);
+    mask.lineTo(videoSize.width * 0.8, 0);
+    mask.lineTo(videoSize.width * 0.65, videoSize.height);
+    mask.lineTo(0, videoSize.height);
+    mask.lineTo(0, 0);
+    mask.fill();
+
+    const maskCanvas = document.createElement("canvas");
+    maskCanvas.width = videoSize.width;
+    maskCanvas.height = videoSize.height;
+    const maskContext = maskCanvas.getContext("2d");
+    maskContext.fillStyle = "#000000";
+    maskContext.fillRect(0, 0, videoSize.width, videoSize.height);
+    // maskContext.filter = "blur(20px)";
+    maskContext.fillStyle = "#ffffff";
+    maskContext.beginPath();
+    maskContext.moveTo(0, 0);
+    maskContext.lineTo(videoSize.width * 0.8, 0);
+    maskContext.lineTo(videoSize.width * 0.65, videoSize.height);
+    maskContext.lineTo(0, videoSize.height);
+    maskContext.lineTo(0, 0);
+    maskContext.fill();
+    // document.body.appendChild(maskCanvas);
+    const maskTexture = PIXI.Texture.from(maskCanvas);
+    const maskSprite = new PIXI.Sprite(maskTexture);
+
+    const blurFilter = new PIXI.BlurFilter({ antialias: true, quality: 5 });
+    blurFilter.strength = 20;
+    blurFilter.repeatEdgePixels = true;
+
+    const imageContainer = new PIXI.Container();
+    imageContainer.label = previewLabel.imageContainer.container;
+    imageContainer.filters = [
+      new DropShadowFilter({
+        offset: new PIXI.Point(0, 0),
+        color: 0x000000,
+        alpha: 1,
+        blur: 7,
+        quality: 15,
+      }),
+    ];
+    app.stage.addChild(imageContainer);
+
+    const colorMatrixFilter = new PIXI.ColorMatrixFilter();
+    colorMatrixFilter.brightness(0.5, false);
+    // colorMatrixFilter.grayscale(0.5, false);
+
+    const calcPoint = (sprite: PIXI.Sprite, point: PIXI.Point): PIXI.Point => {
+      const x = point.x - sprite.width / 2;
+      const y = point.y - sprite.height / 2;
+      return new PIXI.Point(x, y);
+    };
+
     const sprite = new PIXI.Sprite(defaultImage);
-    sprite.label = this.imageLabel;
-    sprite.filters = [new DropShadowFilter()];
-    app.stage.addChild(sprite);
+    sprite.label = previewLabel.imageContainer.image;
+    sprite.filters = [colorMatrixFilter];
+    sprite.mask = maskSprite;
+    sprite.scale.set(factor * 1.5);
+
+    const initPoint = calcPoint(
+      sprite,
+      new PIXI.Point(videoSize.width / 2, videoSize.height / 2)
+    );
+
+    sprite.position.x = initPoint.x;
+    sprite.position.y = initPoint.y;
+    imageContainer.addChild(sprite);
 
     const textContainer = new PIXI.Container({
-      label: this.textContainerLabel,
+      label: previewLabel.textContainer.container,
     });
     textContainer.filters = [
       new DropShadowFilter({
@@ -55,6 +113,7 @@ export class Preview {
       fill: 0xffffff,
       align: "left",
       fontWeight: "700",
+      lineHeight: 96,
     });
 
     Object.values(TextInput).forEach((value, index) => {
@@ -71,7 +130,7 @@ export class Preview {
 
   updateText(app: PIXI.Application, name: string, value: string) {
     const textContainer = app.stage.getChildByName(
-      this.textContainerLabel
+      previewLabel.textContainer.container
     ) as PIXI.Container;
     const text = textContainer.getChildByName(name) as PIXI.Text;
     text.text = value;
@@ -79,8 +138,15 @@ export class Preview {
 
   updateImage(app: PIXI.Application, texture: PIXI.Texture) {
     // TODO: 이미지 자동 스케일링 코드 추가
-    const image = app.stage.getChildByName(this.imageLabel) as PIXI.Sprite;
-    const bgImage = app.stage.getChildByName(this.bgImageLabel) as PIXI.Sprite;
+    const imageContainer = app.stage.getChildByName(
+      previewLabel.imageContainer.container
+    ) as PIXI.Container;
+    const image = imageContainer.getChildByName(
+      previewLabel.imageContainer.image
+    ) as PIXI.Sprite;
+    const bgImage = app.stage.getChildByName(
+      previewLabel.bgImage
+    ) as PIXI.Sprite;
     image.texture = texture;
     bgImage.texture = texture;
   }
