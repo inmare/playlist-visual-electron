c99db5d294b82d85ab9287c731b283c5fa141fb0
inmare <yatsumehole@naver.com>
2024-12-18 11:21:18 +0900

    import문 정리

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,6 +1,8 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import App from "@components/App";
+
 import "@scss/index.scss";
 
 const rootElement = document.getElementById("root");
