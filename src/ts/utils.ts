627138644d2249a4b0a9376ead64141018ef97ae
inmare <yatsumehole@naver.com>
2024-12-19 16:37:17 +0900

    텍스트의 줄 길이를 반환하는 함수 추가

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-import { videoSize } from "./config";
+import { VideoSize } from "./config";
 
 /**
  * 이미지의 넓이가 영상의 넓이에 맞을 수 있도록 비율을 계산하는 함수
@@ -7,5 +7,9 @@ import { videoSize } from "./config";
  * @returns 이미지의 넓이에 곱하면 영상의 넓이가 되는 비율
  */
 export function calculateRatio(image: PIXI.Texture): number {
-  return videoSize.width / image.width;
+  return VideoSize.width / image.width;
+}
+
+export function getLineLength(text: string): number {
+  return text.trim().split("\n").length;
 }
