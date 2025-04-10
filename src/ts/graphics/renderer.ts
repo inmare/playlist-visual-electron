cb7b7c993b4cb5c070207a52c733ece4b72c8f27
inmare <yatsumehole@naver.com>
2025-04-10 10:44:06 +0900

    렌더링 코드 추가

--- src/ts/graphics/renderer.ts
+++ src/ts/graphics/renderer.ts
@@ -1,27 +1,36 @@
 import * as PIXI from "pixi.js";
 import Color from "@ts/utils/color";
+import Layer from "@ts/graphics/layer";
 
 export default class Renderer {
   app: PIXI.Application;
-  layers: Array<any>;
+  layers: Array<Layer>;
 
   constructor(
     app: PIXI.Application,
+    canvas: HTMLCanvasElement,
     width: number,
     height: number,
     bgColor: Color
   ) {
     this.app = app;
     this.app.init({
+      canvas: canvas,
       width: width,
       height: height,
       backgroundColor: bgColor.toHex(),
+      antialias: true,
     });
     this.layers = [];
   }
 
-  addLayer(graphics: any) {
-    this.layers.push(graphics);
-    this.app.stage.addChild(graphics);
+  addLayer(layer: Layer) {
+    this.layers.push(layer);
+  }
+
+  render(time: number) {
+    for (const layer of this.layers) {
+      layer.draw(this.app, time);
+    }
   }
 }
