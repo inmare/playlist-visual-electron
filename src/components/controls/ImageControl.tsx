ff94e44adad162328458e3339a50fdac3fd1575f
inmare <inmare2000@proton.me>
2025-04-10 22:21:54 +0900

    보다 직관적인 컨트롤 컴포넌트 생성

---
+++ src/components/controls/ImageControl.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function ImageControl({
+  label,
+  songIdx,
+}: {
+  label: string;
+  songIdx: number;
+}) {
+  return <></>;
+}
---
+++ src/components/controls/TextControl.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function TextControl({
+  label,
+  songIdx,
+}: {
+  label: string;
+  songIdx: number;
+}) {
+  return <></>;
+}
